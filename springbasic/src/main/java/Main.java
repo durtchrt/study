5ed19516ae6c14b4261e8902321fa42c1b9bd6a7
boojongmin <boojongmin@rtruesoft.kr>
2018-02-22 11:08:09 +0900

    절차지향 예제 - 2
    1. 반복문 추가
    2. 번개속성 마법사 추가

--- springbasic/src/main/java/Main.java
+++ springbasic/src/main/java/Main.java
@@ -2,34 +2,23 @@ public class Main {
     public static void main(String[] args) {
         int m1Hp = 100;
         int m2Hp = 100;
+        int m3Hp = 100;
 
-        m1Hp -= attackByFireball();
-        m2Hp -= attackByIceStorm();
-        System.out.println(String.format("magician 1 HP : %d, magician 2 HP: %d", m1Hp, m2Hp));
+        while(true) {
+            m1Hp -= attackByFireball();
+            m2Hp -= attackByIceStorm();
+            m3Hp -= attackByLightningbolt();
+            System.out.println(String.format("magician 1 HP : %d, magician 2 HP: %d, magician 3 HP: %d", m1Hp, m2Hp, m3Hp));
+            if(m1Hp <= 0 || m2Hp <= 0 || m3Hp <= 0) {
+                System.out.println("종료");
+                break;
+            }
+        }
+    }
 
-        m1Hp -= attackByFireball();
-        m2Hp -= attackByIceStorm();
-        System.out.println(String.format("magician 1 HP : %d, magician 2 HP: %d", m1Hp, m2Hp));
-
-        m1Hp -= attackByFireball();
-        m2Hp -= attackByIceStorm();
-        System.out.println(String.format("magician 1 HP : %d, magician 2 HP: %d", m1Hp, m2Hp));
-
-        m1Hp -= attackByFireball();
-        m2Hp -= attackByIceStorm();
-        System.out.println(String.format("magician 1 HP : %d, magician 2 HP: %d", m1Hp, m2Hp));
-
-        m1Hp -= attackByFireball();
-        m2Hp -= attackByIceStorm();
-        System.out.println(String.format("magician 1 HP : %d, magician 2 HP: %d", m1Hp, m2Hp));
-
-        m1Hp -= attackByFireball();
-        m2Hp -= attackByIceStorm();
-        System.out.println(String.format("magician 1 HP : %d, magician 2 HP: %d", m1Hp, m2Hp));
-
-        m1Hp -= attackByFireball();
-        m2Hp -= attackByIceStorm();
-        System.out.println(String.format("magician 1 HP : %d, magician 2 HP: %d", m1Hp, m2Hp));
+    static int attackByLightningbolt() {
+        System.out.println("라이트닝볼트 발사!!");
+        return 20;
     }
 
     static int attackByFireball() {
