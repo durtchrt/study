aa740950d7511fc70e07879ac9de20376b6bf17f
boojongmin <boojongmin@rtruesoft.kr>
2018-02-22 16:20:46 +0900

    스프링 예제 - 2
    singleton, prototype 예제 추가

--- springbasic/src/main/java/Main.java
+++ springbasic/src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
         StaticApplicationContext context = new StaticApplicationContext();
         context.registerSingleton("m1", FireMagician.class);
         context.registerSingleton("m2", IceMagician.class);
-        context.registerSingleton("m3", LightMagician.class);
+        context.registerPrototype("m3", LightMagician.class);
 
         Magician m1 = (Magician) context.getBean("m1");
         Magician m2 = (Magician) context.getBean("m2");
@@ -25,5 +25,11 @@ public class Main {
                 break;
             }
         }
+
+        Magician m11 = (Magician) context.getBean("m1");
+        Magician m33 = (Magician) context.getBean("m3");
+        System.out.println("m1 == m11 : " + (m1 == m11));
+        System.out.println("m3 == m33 : " + (m3 == m33));
+
     }
 }
